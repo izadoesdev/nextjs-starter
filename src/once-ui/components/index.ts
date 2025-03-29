09a4db1cec18912c829721e360f5224c4a5d9eaa
Adhitya Nadooli <127269031+lightyfr@users.noreply.github.com>
2025-03-29 15:11:25 +0000

    feat: update tooltip formatting to use locale string representation in graph components

--- src/once-ui/components/index.ts
+++ src/once-ui/components/index.ts
@@ -4,7 +4,6 @@ export * from "./Avatar";
 export * from "./AvatarGroup";
 export * from "./Badge";
 export * from "./Background";
-export * from "./BarGraph"
 export * from "./Button";
 export * from "./Carousel";
 export * from "./Card";
@@ -34,11 +33,8 @@ export * from "./InteractiveDetails";
 export * from "./Kbd";
 export * from "./LetterFx";
 export * from "./Line";
-export * from "./LineBarGraph"
-export * from "./LineGraph"
 export * from "./Logo";
 export * from "./LogoCloud";
-export * from "./MultiBarGraph"
 export * from "./NavIcon";
 export * from "./NumberInput";
 export * from "./Option";
